6d7842ab439da7e8db176da7192dc5550e5ea2fd
Natxo <natxetepala@gmail.com>
2025-01-05 05:27:52 +0100

    Initial Commit

---
+++ KeyPressDisplay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeyPressDisplay
+//
+//  Created by Ignacio Palacio  on 5/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeyPressDisplay/KeyPressDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyPressDisplayApp.swift
+//  KeyPressDisplay
+//
+//  Created by Ignacio Palacio  on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct KeyPressDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
